e4001e8f9834053e7c37f5e4a1a3de56627f9ccd
keigo1216 <sagankeigo17@gmail.com>
2024-05-13 13:27:30 +0900

    Fix repository name

--- train.py
+++ train.py
@@ -5,17 +5,19 @@ import os
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', required=True, help='path of model dir')
+    parser.add_argument('--yaml', required=True, help='path of yaml file')
     args = parser.parse_args()
     
     model_path = args.model
+    yaml_path = args.yaml
     
     save_dir = "./run"
     os.makedirs(save_dir, exist_ok=True)
     
     model = YOLO(model_path)  # Load model
     model.train(
-        data='yolo.yaml',
-        epochs=500, 
-        batch=16,
+        data=yaml_path,
+        epochs=2000, 
+        batch=32,
         project=save_dir,
     )
